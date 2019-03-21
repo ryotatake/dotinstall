287c91668c7489dbb9d819c0b036b5ba35379473
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-21 22:43:54 +0000

    #03 オブジェクトを作ってみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,12 +1,9 @@
 'use strict';
 
 {
-  const scores = [80, 90, 40];
-  console.log( scores );
-  console.log( scores[1] );
-
-  scores[2] = 100;
-  console.log( scores );
-
-  console.log( scores.length );
+  const player = {
+    name: 'taguchi',
+    score: 32,
+  }
+  console.log( player );
 }
