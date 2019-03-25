37a425922693580bda7da27e547b70d07f58f071
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-25 12:16:05 +0000

    #04 オブジェクトを操作してみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -5,5 +5,15 @@
     name: 'taguchi',
     score: 32,
   }
+  console.log( player.name );
+  console.log( player['name'] );
+
+  player.score = 100;
+  console.log( player );
+
+  player.email = 'taguchi@gmail.com';
+  console.log( player.email );
+
+  delete player.score;
   console.log( player );
 }
