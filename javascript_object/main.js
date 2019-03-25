d3175dd35489c6b5d9a0412e9af8b8fa38af13d1
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-25 12:22:33 +0000

    #06 メソッドを使ってみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -10,5 +10,22 @@
     constructor(name, score) {
       this.name = name;
       this.score = score;
+    }
+
+    showInfo() {
+      console.log( `name: ${ this.name } score: ${ this.score }` );
+    }
+
+    static showVersion() {
+      console.log( 'Player class ver. 1.0' );
+    }
   }
+
+  const taguchi = new Player( 'taguchi', 32 );
+  const fkoji = new Player( 'fkoji', 44 );
+
+  taguchi.showInfo();
+  fkoji.showInfo();
+
+  Player.showVersion();
 }
