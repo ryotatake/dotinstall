abb8b6aa4b00e749da8b7e8d55cc0ccc8f002701
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-28 12:04:45 +0000

    #13 オブジェクトを操作してみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,20 +1,16 @@
 'use strict';
 
 {
-  const a = [1, 2, 5, 10];
-  const b = [];
-  a.forEach( item => {
-    b.push( item * 2 );
+  const o = {
+    a: 1,
+    b: 2,
+  };
+
+  console.log( Object.keys( o ) );
+  console.log( Object.values( o ) );
+  console.log( Object.entries( o ) );
+
+  Object.keys( o ).forEach( key => {
+    console.log( `${key}: ${o[key]}`);
   });
-  console.log( b );
-
-  const c = a.map( item => {
-    return item * 2;
-  });
-
-  const d = a.map( item => item * 2 );
-  console.log( d );
-
-  const e = a.filter( item => item % 2 === 0 );
-  console.log( e );
 }
