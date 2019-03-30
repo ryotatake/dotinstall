4aec31ae5e713b52a9d855eb149974fe03cfaf5c
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-30 02:20:35 +0000

    #16 文字列を操作してみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,27 +1,9 @@
 'use strict';
 
 {
-  // const numbers = [1, 2];
-  // // const a = numbers[0];
-  // // const b = numbers[1];
-  // const [a, b] = numbers;
-  // console.log( a );
-  // console.log( b );
+  const str = 'hello';
 
-  // const numbers = [1, 2, 3, 8];
-  // const [a, b, ...rest] = numbers;
-  // console.log( a );
-  // console.log( b );
-  // console.log( rest );
-
-  const player = {
-    name: 'taguchi',
-    score: 55,
-    hp: 33,
-    mp: 22,
-  };
-  const {name, score, ...points} = player;
-  console.log( name );
-  console.log( score );
-  console.log( points );
+  console.log( str.length ); // 5
+  console.log( str.substring( 2, 4 ) );
+  console.log( str[1] );
 }
