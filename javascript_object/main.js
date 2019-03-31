b4e3dd3dcdd237e1901348f9b70b10fbec8054b4
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-31 08:41:09 +0000

    #20 alert, confirmを使ってみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,14 +1,9 @@
 'use strict';
 
 {
-  // const d = new Date( 2018, 11 ); // 2018/12/01 00:00:00
-  // d.setHours( 10, 20, 30 ); // 2018/12/01 10:20:30
-  // d.setDate( 32 ); // 2019/01/01 10:20:30
-  // d.setDate( d.getDate() + 3 );
+  //window.alert( 'hello' );
+  // alert( 'hello' );
 
-  // console.log( d );
-
-  const d1 = new Date( 2018, 11, 1 );
-  const d2 = new Date( 2018, 11, 10 );
-  console.log( ( d2 -d1 ) / ( 24 * 60 * 60 * 1000 ) );
+  const answer = confirm( 'Are you sure?' );
+  console.log( answer );
 }
