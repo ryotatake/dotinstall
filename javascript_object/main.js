aaf0534cfc6dd1da64d782fe79e7fc586c4a792a
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-31 08:39:10 +0000

    #19 Dateオブジェクトを操作してみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
 {
-  const d = new Date();
-  console.log( d );
+  // const d = new Date( 2018, 11 ); // 2018/12/01 00:00:00
+  // d.setHours( 10, 20, 30 ); // 2018/12/01 10:20:30
+  // d.setDate( 32 ); // 2019/01/01 10:20:30
+  // d.setDate( d.getDate() + 3 );
 
-  console.log( d.getFullYear() );
-  console.log( d.getMonth() ); // 0 - 11
-  console.log( d.getDate() ); // 0 - 6
-  console.log( d.getHours() );
-  console.log( d.getMinutes() );
-  console.log( d.getSeconds() );
-  console.log( d.getMilliseconds() );
+  // console.log( d );
 
-  console.log( d.getTime() );
+  const d1 = new Date( 2018, 11, 1 );
+  const d2 = new Date( 2018, 11, 10 );
+  console.log( ( d2 -d1 ) / ( 24 * 60 * 60 * 1000 ) );
 }
