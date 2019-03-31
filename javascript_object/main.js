aa2d684ff5c9565caac40f48f5124c1369e9c119
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-31 08:35:31 +0000

    #18 Dateオブジェクトを使ってみよう

--- javascript_object/main.js
+++ javascript_object/main.js
@@ -1,8 +1,16 @@
 'use strict';
 
 {
-  console.log( Math.PI );
-  console.log( Math.random() );
+  const d = new Date();
+  console.log( d );
 
-  console.log( Math.floor( Math.random() * 6 + 1 ) );
+  console.log( d.getFullYear() );
+  console.log( d.getMonth() ); // 0 - 11
+  console.log( d.getDate() ); // 0 - 6
+  console.log( d.getHours() );
+  console.log( d.getMinutes() );
+  console.log( d.getSeconds() );
+  console.log( d.getMilliseconds() );
+
+  console.log( d.getTime() );
 }
