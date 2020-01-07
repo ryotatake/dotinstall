21776a5f6fd264ba3df8c9aa27d48d86830fcf10
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-01-07 21:12:39 +0900

    プラグインファイルを作ろう

---
+++ jquery_plugin/jquery.showsize.js
@@ -0,0 +1,18 @@
+;(function($) {
+  $.fn.showsize = function() {
+    // $('img').click(function() {
+    //   let msg = $(this).width() + ' x ' + $(this).height();
+    //   $(this).wrap('<div style="position: relative;"></div>');
+    //   let div = $("<div>")
+    //     .text(msg)
+    //     .css('position', 'absolute')
+    //     .css('top', '0')
+    //     .css('background', 'black')
+    //     .css('color', 'white')
+    //     .css('font-size', '10px')
+    //     .css('opacity', '0.9')
+    //     .css('padding', '2px')
+    //   $(this).after(div);
+    // });
+  }
+})(jQuery);
