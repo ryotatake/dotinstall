1f553bd9a3e3d87893cba129b81de10e59f3988f
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-01-07 21:26:58 +0900

    プラグインを動作させよう

--- jquery_plugin/jquery.showsize.js
+++ jquery_plugin/jquery.showsize.js
@@ -1,18 +1,26 @@
 ;(function($) {
   $.fn.showsize = function() {
-    // $('img').click(function() {
-    //   let msg = $(this).width() + ' x ' + $(this).height();
-    //   $(this).wrap('<div style="position: relative;"></div>');
-    //   let div = $("<div>")
-    //     .text(msg)
-    //     .css('position', 'absolute')
-    //     .css('top', '0')
-    //     .css('background', 'black')
-    //     .css('color', 'white')
-    //     .css('font-size', '10px')
-    //     .css('opacity', '0.9')
-    //     .css('padding', '2px')
-    //   $(this).after(div);
-    // });
-  }
+    // this(3つのimg)
+    let elements = this; // 分かりやすいように名付け
+
+    elements.each(function(idx, element) {
+      // this (1つのimg)
+      $(element).click(function() {
+        let msg = $(this).width() + ' x ' + $(this).height();
+        $(this).wrap('<div style="position: relative;"></div>');
+        let div = $("<div>")
+          .text(msg)
+          .css('position', 'absolute')
+          .css('top', '0')
+          .css('background', 'black')
+          .css('color', 'white')
+          .css('font-size', '10px')
+          .css('opacity', '0.9')
+          .css('padding', '2px')
+        $(this).after(div);
+      });
+    });
+
+    return this; //メソッドチェインできるように
+  };
 })(jQuery);
